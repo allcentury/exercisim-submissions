4e6a53745e44dcfb7e3f4e1d3db4d7679af99f11
Anthony Ross <anthony.s.ross@gmail.com>
2014-12-01 13:34:24 -0500

    Added go version for clock

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,6 +1,30 @@
 package clock
 
-type Clock string;
+import "fmt"
 
+const TestVersion = 1
 
+type Clock struct {
+	hour, minute int
+}
 
+func New(hour, minute int) Clock {
+	baseHour := hour % 24
+	minutesPerHour := baseHour * 60
+	minutesIn24Hours := 1440
+
+	totalMinutes := minutesPerHour + minutesIn24Hours + minute
+
+	newHour := (totalMinutes / 60 ) % 24
+	newMinute := totalMinutes % 60
+
+	return Clock{newHour, newMinute}
+}
+
+func (c Clock) String() string {
+	return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
+}
+
+func (c Clock) Add(m int) Clock {
+	return New(c.hour, c.minute+m)
+}
